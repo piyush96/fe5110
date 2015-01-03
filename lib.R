64a61fc75b4931e7a09fe9e8348baac6ce528c37
lvbach88 <lvbach88@gmail.com>
2015-01-03 10:00:27 +0700

    create ArimaTR function rename myATR to CalculateATR

--- lib.R
+++ lib.R
@@ -1,15 +1,24 @@
-# Library
-plusOne <- function(x){
-    x = x + 1
-    return (x)
-}
+## Constants
+RANGE = 250 # number of days in True Range time series to construct ARiMA
+RANGE_RATO = 2/3 # portion of whole duration to construct ARiMA
+ATR_DAYS = 20 # number of days to compute ATR
 
-myATR <- function(codeName){
-    codeData = Quandl(code = codeName, type = "xts")
+## Library
+CalculateATR <- function(p.code.data){ # p.code.data is a xts returned from Quandl
+    #p.code.data = Quandl(code = _codeName, type = "xts")
     #class(codeData)
-    colnames(codeData)[6] = "Close"
-    atr = ATR(codeData[,c("High","Low","Close")], n=14)
+    colnames(p.code.data)[6] = "Close"
+    p.atr = ATR(p.code.data[,c("High","Low","Close")], n=ATR_DAYS)
     #class(atr)
     #codeData[,"Close"]
-    return (atr)
+    return (p.atr)
+}
+
+ArimaTR <- function(p.atr){ #p.atr is the return of CalculateATR
+    p.ndays = paste(nrow(p.atr)-1, "days", sep = " ")
+    p.range = paste(floor(nrow(p.atr)*RANGE_RATO), "days", sep = " ") #range should be long enough, it is now 2/3 of the entire duration
+    #p.range = paste(RANGE, "days", sep = " ")
+    p.tr = as.vector(first(last(p.atr, p.ndays), p.range)[, "tr"])
+    p.autoTR = auto.arima(p.tr, max.p = 20, max.q = 0, ic = "aic")
+    return (p.autoTR)
 }
--- main.R
+++ main.R
@@ -17,43 +17,18 @@ source("lib.R")
 ###############
 
 ## main
-#hof2013.data <- read.csv(file = "CME-HOF2013.csv", header = TRUE, sep = ",")
-#colnames(hof2013.data)[7] = "Close"
-#mhof2013 = as.matrix(hof2013.data)
-#atr <- ATR(mhof2013[,c("High","Low","Close")], n=14)
 hof2005 = Quandl(code = "CME/HOF2005", type = "xts")
-class(hof2005)
-colnames(hof2005)[6] = "Close"
-atr = ATR(hof2005[,c("High","Low","Close")], n=14)
-class(atr)
-hof2005[,"Close"]
-acf(atr['2003-07-02/'][,"tr"])
+#class(hof2005)
+#acf(atr['2003-07-02/'][,"tr"])
+atr_hof2005 = CalculateATR(hof2005)
+autoTrHof2005 = ArimaTR(atr_hof2005)
+autoTrHof2005
+Box.test(autoTrHof2005$resid,lag=9,fitdf=8)
 
-# coreTr = coredata(tr)
-# plot(coreTr[1:372], coreTr[2:373])
-# dCoreTr = coreTr[2:373] - coreTr[1:372]
-# plot(dCoreTr[1:371], dCoreTr[2:372])
 
-# auto.arima(diff(x),max.p=20,max.q=0,ic="aic")
-# data(bmw,package="evir")
-# bmw = as.vector(bmw)
-# n=length(bmw)
-# 
-# fitAR1 = arima(bmw, order = c(1,0, 0))
-# Box.test(fitAR1$resid,lag=2,fitdf=1)
-coreTr = coredata(atr['2003-07-02/'][,"tr"])
-acf(x = diff(coreTr))
-p = 5
-fitTR = arima(as.vector(diff(coreTr)), order = c(p, 0, 0))
-Box.test(fitTR$resid,lag=p+1,fitdf=p)
-autoTr = auto.arima(as.vector( coreTr), max.p = 20, max.q = 0, ic = "aic")
-autoTr
-Box.test(autoTr$resid,lag=10,fitdf=9)
-
-atr_hok2002 = myATR("CME/HOK2002")
-ndays = paste(nrow(atr_hok2002)-1, "days", sep = " ")
-range = paste(floor(nrow(atr_hok2002)/3*2), "days", sep = " ") #range should be long enough, it is now 2/3 of the entire duration
-tr_hok2002 = as.vector(first(last(atr_hok2002, ndays), range)[, "tr"])
-autoTrHok2002 = auto.arima(tr_hok2002, max.p = 20, max.q = 0, ic = "aic")
+hok2002 = Quandl(code = "CME/HOK2002", type = "xts")
+atr_hok2002 = CalculateATR(hok2002)
+autoTrHok2002 = ArimaTR(atr_hok2002)
 autoTrHok2002
+
 Box.test(autoTrHok2002$resid,lag=3,fitdf=2)
